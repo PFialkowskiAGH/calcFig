8ed05c7149f1eaf0e1fce17c654a335ae1025082
Patryk <patfial1@lab.metal.agh.edu.pl>
2023-03-12 18:16:12 +0100

    nearly ended triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,17 +1,30 @@
+import java.util.Locale;
+
 public class Triangle extends Figure implements Printable
 {
+    double a, b, c;
+    //constructor
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    //P=p(p−a)(p−b)(p−c)−−−−−−−−−−−−−−−−−√
     @Override
     double calculateAREA() {
-        return 0;
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     @Override
     double calculatePerimeter() {
-        return 0;
+        return a + b + c;
     }
 
     @Override
     public void print() {
-
+        System.out.println(String.format(Locale.US,%.2f %.2f %.2f, a, b, c));
+        System.out.println(this.calculateAREA());
+        System.out.println(this.calculatePerimeter());
     }
 }
