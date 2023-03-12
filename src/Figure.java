7beee6c23e9b946122910b6d8c5f1292a47b6ff6
Patryk <patfial1@lab.metal.agh.edu.pl>
2023-03-12 17:52:28 +0100

    add src

---
+++ src/Figure.java
@@ -0,0 +1,5 @@
+public abstract class Figure {
+    abstract double calculateAREA();
+    abstract double calculatePerimeter();
+}
+
---
+++ src/Square.java
@@ -0,0 +1,17 @@
+public class Square extends Figure implements Printable
+{
+    @Override
+    double calculateAREA() {
+        return 0;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
