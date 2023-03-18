8fa310e25a8fcb0eeb8633939cc75d18a8c525fc
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-03-18 15:36:17 +0100

    Change override method toString in Class Square, Triangle and Add Class Circle nad Prism

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,25 @@
-public class Circle {
+public class Circle extends Figure implements Printable {
+    double r;
+
+    //constructor
+    public Circle(double r) { this.r = r; }
+
+    @Override
+    public String toString()
+    {
+        return "Okrąg o promieniu: %.2f".formatted(r);
+    }
+
+    @Override
+    double calculateAREA() { return Math.pow(r,2) * Math.PI; }
+
+    @Override
+    double calculatePerimeter() { return 2 * r * Math.PI; }
+
+    @Override
+    public void print() {
+        System.out.println(this);
+        System.out.printf("Pole: %.2f%n", this.calculateAREA());
+        System.out.printf("Obwód: %.2f%n", this.calculatePerimeter());
+    }
 }
--- src/Prism.java
+++ src/Prism.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Prism {
+public class Prism extends Figure implements Printable {
+    double H;
+    Figure base;
+
+    //constructor
+    public Prism(Figure base, double H) {
+        this.H = H;
+        this.base = base;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Graniastosłup prawidłowy:\n Wysokość: %.2f\n Podstawa: %s".formatted(H, this.base.toString());
+    }
+
+    @Override
+    double calculateAREA() { return this.base.calculateAREA() * H; }
+
+    @Override
+    double calculatePerimeter() { return 2 * this.base.calculateAREA() + this.base.calculatePerimeter() * H; }
+
+    @Override
+    public void print() {
+        System.out.println(this);
+        System.out.printf("Pole: %.2f%n", this.calculateAREA());
+        System.out.printf("Obwód: %.2f%n", this.calculatePerimeter());
+    }
 }
